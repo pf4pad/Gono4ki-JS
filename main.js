b73d1a46d1ecee01056d949dac9666831b1c275e
pf4pad <65763670+pf4pad@users.noreply.github.com>
2021-06-22 22:36:33 +0300

    gono4ki-1

---
+++ main.js
@@ -0,0 +1,52 @@
+const score = document.querySelector('.score'),
+start = document.querySelector('.start'),
+gameArea = document.querySelector('.gameArea'),
+car = document.createElement('div');
+
+car.classList.add('car')
+start.addEventListener ('click', startGame)
+document.addEventListener('keyup', startRun)
+document.addEventListener('keydown', stopRun)
+
+const keys = {
+ArrowUp: false,
+ArrowDown: false,
+ArrowRight: false,
+ArrowLeft: false
+};
+
+const setting = {
+  start: false,
+  score: 0,
+  speed: 3
+};
+
+function startGame(){
+    start.classList.add('hide');
+    gameArea.appendChild(car);
+    setting.start = true
+    requestAnimationFrame(playGame)
+    
+}
+
+function playGame(){
+  console.log('Play Game');
+  if(setting.start){
+      requestAnimationFrame(playGame);
+    }
+  
+}
+
+function startRun(event){
+  event.preventDefault();
+  keys[event.key] = true;
+}
+
+function stopRun(event){
+    event.preventDefault();
+    keys[event.key] = false;
+    console.log(event.key)
+}
+
+
+
