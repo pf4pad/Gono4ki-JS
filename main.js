8aaf1dc8b1b8ea7bbce9bfff40fb16bedaf66ffe
pf4pad <65763670+pf4pad@users.noreply.github.com>
2021-06-24 08:17:13 +0300

    gono4ki-badArrow

--- main.js
+++ main.js
@@ -32,7 +32,9 @@ function getQuantityElements(heightElement){
 
 function startGame(){
     start.classList.add('hide');
-
+    gameArea.innerHTML = '';
+    
+    
     for(let i = 0; i < getQuantityElements(100); i++){
       const line = document.createElement('div');
       line.classList.add('line');
@@ -50,18 +52,24 @@ function startGame(){
       enemy.style.background = 'transparent url("./image/enemy.png") center / cover no-repeat';
       gameArea.appendChild(enemy);
     }
-
-    setting.start = true;
+  setting.score = true;
+  setting.start = true;
   gameArea.appendChild(car);
+  car.style.left = gameArea.offsetWidth/2 - car.offsetWidth/2;
+  car.style.top = 'auto';
+  car.style.bottom = '10px'
   setting.x = car.offsetLeft;
   setting.y = car.offsetTop;
   requestAnimationFrame(playGame);
 }
 
 function playGame() {
-  moveRoad();
-  moveEnemy();
+  
   if (setting.start) {
+    moveRoad();
+    moveEnemy();
+    setting.score += setting.speed;
+    score.innerHTML = 'SCORE<br> ' + setting.score;
     if (keys.ArrowLeft && setting.x > 0) {
       setting.x -= setting.speed;
     }
@@ -111,6 +119,21 @@ function moveRoad(){
 function moveEnemy(){
   let enemy = document.querySelectorAll('.enemy');
   enemy.forEach(function(item){
+
+    let carRect = car.getBoundingClientRect();
+    let enemyRect = item.getBoundingClientRect();
+
+
+      if(carRect.top <= enemyRect.bottom && 
+         carRect.right >= enemyRect.left &&
+         carRect.left <= enemyRect.right && 
+         carRect.bottom >= enemyRect.top){
+          setting.start = false;
+           console.warm('lng');
+           start.classList.remove('hide');
+           score.style.top = start.offsetHeight
+         }
+      
     item.y += setting.speed / 2;
     item.style.top = item.y + 'px'
      if (item.y >= document.documentElement.clientHeight){
@@ -118,7 +141,6 @@ function moveEnemy(){
        item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
       }
   });
- 
-}
+ }
 
 
